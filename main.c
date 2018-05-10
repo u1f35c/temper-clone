f259a83606c7e6692970cd0ec814a7414a481938
Jonathan McDowell <noodles@earth.li>
2018-05-10 08:04:53 +0100

    Do USB disconnect/reconnect dance before initialising USB
    According to http://vusb.wikidot.com/driver-api the initial
    usbDeviceDisconnect() / usbDeviceConnect() for enumeration should
    be called before doing the usbInit(). Haven't seen problems as a
    result, but do so.

--- main.c
+++ main.c
@@ -375,16 +375,15 @@ int main(void)
 	w1_setup();
 	set_serial();
 
-	usbInit();
 	usbDeviceDisconnect();
-
 	i = 0;
 	while (--i) {
 		wdt_reset();
 		_delay_ms(1);
 	}
-
 	usbDeviceConnect();
+
+	usbInit();
 
 	/* PB1 as output for LED */
 	DDRB |= 1 << PB1;
